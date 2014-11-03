39d010cb9db0a91a960b047c49ac3934623dde71
Stephen Abrams <sabrams@trumedianetworks.com>
2014-11-03 18:07:48 -0500

    different behavior for VO's vs. entities

--- tests/domain-entities-test.js
+++ tests/domain-entities-test.js
@@ -7,8 +7,10 @@ describe('model creation', function () {
     it('should throw error when "type" is not a Date, String, or Number and is not a model with a create function', function () {
         var modelFactory = ModelFactory.create({});
         modelFactory.create.bind(modelFactory, {
-            homeAddress: {
-                type: 'Address'
+            attrs: {
+                homeAddress: {
+                    type: 'Address'
+                }
             }
         }).should.throw('Invalid type specified for: homeAddress');
     })
@@ -25,42 +27,51 @@ describe('created model', function () {
             it('should throw error when required field is missing', function () {
                 var modelFactory = ModelFactory.create({});
                 var Person = modelFactory.create({
-                    firstName: {
-                        required: true,
-                        type: String
+                    attrs: {
+                        firstName: {
+                            required: true,
+                            type: String
+                        }
                     }
                 });
                 Person.create.bind(Person, {}).should.throw('Required field: firstName');
             });
-            it('should bubble up error from invalid VO creation when data is passed for VO attribute', function() {
+            it('should bubble up error from invalid VO creation when data is passed for VO attribute', function () {
                 var modelFactory = ModelFactory.create({});
                 var Address = modelFactory.create({
-                    street: {
-                        type: String
-                    },
-                    city: {
-                        type: String
-                    },
-                    state: {
-                        type: String
-                    },
-                    country: {
-                        type: String,
-                        required: true
+                    valueObject: true,
+                    attrs: {
+                        street: {
+                            type: String
+                        },
+                        city: {
+                            type: String
+                        },
+                        state: {
+                            type: String
+                        },
+                        country: {
+                            type: String,
+                            required: true
+                        }
                     }
                 });
                 var Person = modelFactory.create({
-                    homeAddress: {
-                        type: Address
+                    attrs: {
+                        homeAddress: {
+                            type: Address
+                        }
                     }
                 });
 
-                Person.create.bind( Person, {homeAddress: {
-                    street: '123 1st St.',
-                    city: 'anywhere',
-                    state: 'ohio'
-                    // missing country
-                }}).should.throw('Required field: country');
+                Person.create.bind(Person, {
+                    homeAddress: {
+                        street: '123 1st St.',
+                        city: 'anywhere',
+                        state: 'ohio'
+                        // missing country
+                    }
+                }).should.throw('Required field: country');
             })
         });
 
@@ -71,13 +82,42 @@ describe('created model', function () {
     })
 });
 
+describe('created value object', function() {
+    it('should not allow an id to be set or retrieved if not part of the VO\'s attributes', function() {
+        var modelFactory = ModelFactory.create({});
+        var Address = modelFactory.create({
+            valueObject: true,
+            attrs: {
+                street: {
+                    type: String
+                },
+                city: {
+                    type: String
+                },
+                state: {
+                    type: String
+                },
+                country: {
+                    type: String
+                }
+            }
+        });
+        var address = Address.create({
+            country: "US"
+        });
+        (address.getId === undefined).should.be.true;
+        (address.setId === undefined).should.be.true;
+    });
+});
 describe('created entity', function () {
     it('should not have directly visible attributes', function () {
         var modelFactory = ModelFactory.create({});
         var Person = modelFactory.create({
-            firstName: {
-                required: true,
-                type: String
+            attrs: {
+                firstName: {
+                    required: true,
+                    type: String
+                }
             }
         });
         var sam = Person.create({firstName: 'sam'});
@@ -86,59 +126,141 @@ describe('created entity', function () {
     it('should include simple attributes in toJSON result, toData result', function () {
         var modelFactory = ModelFactory.create({});
         var Person = modelFactory.create({
-            firstName: {
-                required: true,
-                type: String
+            attrs: {
+                firstName: {
+                    required: true,
+                    type: String
+                }
             }
         });
         var sam = Person.create({firstName: 'sam'});
         JSON.parse(sam.toJSON()).should.eql({
             firstName: 'sam'
-        })
+        });
         sam.toData().should.eql({
             firstName: 'sam'
         })
     });
+    it('should allow an id (string or number) to be set on it and retrieved from it', function() {
+        var modelFactory = ModelFactory.create({});
+        var Person = modelFactory.create({
+            attrs: {
+                firstName: {
+                    type: String
+                }
+            }
+        });
+
+        var person = Person.create({});
+        person.setId("1234");
+        person.getId().should.eql("1234");
+
+        person.setId(1234);
+        person.getId().should.eql(1234);
+
+//        person.setId({
+//            id: 1234
+//        });
+//        person.getId().should.eql({
+//            id: 1234
+//        });
+
+    });
+    it('should not expose an id directly', function() {
+        var modelFactory = ModelFactory.create({});
+        var Person = modelFactory.create({
+            attrs: {
+                firstName: {
+                    type: String
+                }
+            }
+        });
+
+        var person = Person.create({});
+        person.setId("1234");
+        person.getId().should.eql("1234");
+
+        person.setId(1234);
+        person.getId().should.eql(1234);
+
+        person.setId({
+            id: 1234
+        });
+        (person.id === undefined).should.be.true;
+    });
+    it('should include id in toJSON result, toData result', function() {
+        var modelFactory = ModelFactory.create({});
+        var Person = modelFactory.create({
+            attrs: {
+                firstName: {
+                    type: String
+                }
+            }
+        });
+        var person = Person.create({
+            firstName: "Bob"
+        });
+        person.setId("1234");
+        person.toData().should.eql({
+            firstName: "Bob",
+            id: "1234"
+        });
+        JSON.parse(person.toJSON()).should.eql({
+            firstName: "Bob",
+            id: "1234"
+        })
+    })
     it('should include value object attributes in toJSON result, toData result', function () {
         var modelFactory = ModelFactory.create({});
 
         var Address = modelFactory.create({
-            street: {
-                type: String
-            },
-            city: {
-                type: String
-            },
-            state: {
-                type: String
-            },
-            country: {
-                type: String
+            valueObject: true,
+            attrs: {
+                street: {
+                    type: String
+                },
+                city: {
+                    type: String
+                },
+                state: {
+                    type: String
+                },
+                country: {
+                    type: String
+                }
             }
         });
 
         var Person = modelFactory.create({
-            homeAddress: {
-                type: Address
+            attrs: {
+                homeAddress: {
+                    type: Address
+                }
             }
         });
-        var personWithAddress = Person.create({homeAddress: {
-            street: '123 1st St.',
-            city: 'anywhere',
-            state: 'ohio',
-            country: 'US'
-        }});
-        personWithAddress.toData().should.eql({homeAddress: {
-            street: '123 1st St.',
-            city: 'anywhere',
-            state: 'ohio',
-            country: 'US'
-        }})
-        JSON.parse(personWithAddress.toJSON()).should.eql({homeAddress: {
-            street: '123 1st St.',
-            city: 'anywhere',
-            state: 'ohio',
-            country: 'US'
-        }});
+        var personWithAddress = Person.create({
+            homeAddress: {
+                street: '123 1st St.',
+                city: 'anywhere',
+                state: 'ohio',
+                country: 'US'
+            }
+        });
+        personWithAddress.toData().should.eql({
+            homeAddress: {
+                street: '123 1st St.',
+                city: 'anywhere',
+                state: 'ohio',
+                country: 'US'
+            }
+        });
+        JSON.parse(personWithAddress.toJSON()).should.eql({
+            homeAddress: {
+                street: '123 1st St.',
+                city: 'anywhere',
+                state: 'ohio',
+                country: 'US'
+            }
+        });
     })
 });
